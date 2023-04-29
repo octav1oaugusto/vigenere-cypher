d5eb8e797d124963e96f468f159518b79c15d203
Silva <octavio.silva@vetta.digital>
2023-04-29 11:53:47 -0300

    Add decryption class

--- src/cifra-vigenere/vigenere.py
+++ src/cifra-vigenere/vigenere.py
@@ -3,6 +3,7 @@ from tools.cifra_tools import Tools
 class Cifra(Tools):
     alfabeto = "abcdefghijklmnopqrstuvwxyz"
     CRIPTOGRAFAR = False
+    DESCRIPTOGRAFAR = False
     texto_processado = ""
     alfabeto_deslocado = ""
 
@@ -26,23 +27,36 @@ class Cifra(Tools):
 
     def adaptador(self, caracter):
         if self.CRIPTOGRAFAR:
+            print("cri")
             self.criptografar(caracter)
+        if self.DESCRIPTOGRAFAR: 
+            print("decri")
+            self.descriptografar(caracter)
 
     def criptografar(self, caracter):
         indice_p = self.alfabeto.find(caracter)
         self.texto_processado = (
             self.texto_processado + self.alfabeto_deslocado[indice_p]
         )
+    
+    def descriptografar(self, caracter):
+        indice_p = self.alfabeto_deslocado.find(caracter)
+        self.texto_processado = self.texto_processado + self.alfabeto[indice_p]
         
 class Cifrador(Cifra):
     def __init__(self, texto, senha):
         self.CRIPTOGRAFAR = True
-        super().__init__(senha, texto)
+        super().__init__(texto, senha)
 
+class Decifrador(Cifra):
+    def __init__(self, texto, senha):
+        self.DESCRIPTOGRAFAR = True
+        super().__init__(texto, senha)
 
+        
 # teste de entradas    
 texto = input("insira o texto: ")
 senha = input("insira a senha: ")
 
-cifra = Cifrador(texto, senha)
+cifra = Decifrador(texto, senha)
 print(f"texto: {cifra}")
