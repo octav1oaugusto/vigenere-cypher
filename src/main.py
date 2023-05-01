813e2a66d126ce7085e3e6bda754b2fe03ec7b11
Silva <octavio.silva@vetta.digital>
2023-05-01 15:19:57 -0300

    include decryption after find key

--- src/main.py
+++ src/main.py
@@ -3,14 +3,19 @@ from cypher.attack import AtaqueAnaliseFrequenciaENG, AtaqueAnaliseFrequenciaPT
 
 if __name__ == "__main__":
   option = 0
+  separator = "=" * 20
+  space = ""
   while option != 4:
+    print(space)
+    print(separator)
     print("MENU")
-    print("="*12)
+    print(separator)
     print("1-Cifrar mensagem")
     print("2-Decifrar mensagem")
     print("3-Atacar a cifra")
     print("4-Encerrar programa")
-    
+    print(separator)
+    print(space)    
     option= input("Escolha uma opção: ")
     if option == "1":
       string_ofc = input("Insira a mensagem decifrada: ")
@@ -27,14 +32,40 @@ if __name__ == "__main__":
         language = input("Escolha a linguagem: ")[0].lower()
         if language == "p":
           string_ofc = input("Insira uma cifra: ")
-          for key in reversed(AtaqueAnaliseFrequenciaPT(string_ofc).get_keys()):
-            print("")
-            print("Found key: {!r}".format(key))
+          key = AtaqueAnaliseFrequenciaPT(string_ofc).get_keys()[-1]
+          print(space)
+          print(separator)
+          print(f"Chave encontrada: {key}")
+          print(separator)
+          print(space)
+          decipher = input("Você deseja decifrar a cifra com essa chave? (Sim/Não) ")[0].lower()  
+          if decipher == "s":
+            print(space)
+            print(separator)
+            print("Decrypted message: ")
+            print(separator)
+            print(Decifrador(string_ofc, key))
+            print(separator)
+          elif decipher == "n":
+            break
         if language == "i":
           string_ofc = input("enter a cipher: ")
-          for key in reversed(AtaqueAnaliseFrequenciaENG(string_ofc).get_keys()):
-            print("")
-            print("Found key: {!r}".format(key))  
+          key = AtaqueAnaliseFrequenciaENG(string_ofc).get_keys()[-1]
+          print(space)
+          print(separator)
+          print(f"Found key: {key}")
+          print(separator)
+          print(space)
+          decipher = input("Do you want to decrypt the cipher with that key? (Yes/Not) ")[0].lower()
+          if decipher == "y":
+            print(space)
+            print(separator)
+            print("Decrypted message: ")
+            print(separator)
+            print(Decifrador(string_ofc, key))
+            print(separator)
+          elif decipher == "n":
+            break
         if language == "m": 
           break
     elif option == "4":
